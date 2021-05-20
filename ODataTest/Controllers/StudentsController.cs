79f7b7fa8eefea0207219a418c42b58f957f0278
Paul Ruddock <paul@ruddock.me.uk>
2021-05-20 22:13:25 +0100

    Add project files.

---
+++ ODataTest/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using ODataTest.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ODataTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            return Ok(new List<Student>
+            { 
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Paul Ruddock",
+                    Grade = 1
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Tim Stone",
+                    Grade = 2
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Stephen Blair",
+                    Grade = 3
+                }
+            });
+        }
+    }
+}
---
+++ ODataTest/Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNet.OData.Formatter;
+using Microsoft.OData.Edm;
+using Microsoft.AspNet.OData.Builder;
+using ODataTest.Models;
+
+namespace ODataTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddOData();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ODataTest", Version = "v1" });
+            });
+
+            AddFormatters(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ODataTest v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.EnableDependencyInjection();
+                endpoints.Select().Filter().Expand();
+                endpoints.MapODataRoute(routeName: "odata", routePrefix: "odata", model: GetEdmModel());
+              
+            });
+        }
+
+        IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Student>("Student");
+            return builder.GetEdmModel();
+        }
+
+        private void AddFormatters(IServiceCollection services)
+        {
+            services.AddMvcCore(options =>
+            {
+                IEnumerable<ODataOutputFormatter> outputFormatters =
+                    options.OutputFormatters.OfType<ODataOutputFormatter>()
+                        .Where(foramtter => foramtter.SupportedMediaTypes.Count == 0);
+
+                foreach (var outputFormatter in outputFormatters)
+                {
+                    outputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/odata"));
+                }
+            });
+        }
+    }
+}
